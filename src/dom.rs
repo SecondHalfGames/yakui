69687769afdb027a17ee95790880968b2a5e91cd
Lucien Greathouse <me@lpghatguy.com>
2022-07-03 01:35:42 -0400

    Track parent nodes in WorkItem

--- src/dom.rs
+++ src/dom.rs
@@ -35,15 +35,26 @@ impl Dom {
     }
 
     pub fn apply(&mut self, snapshot: Snapshot) {
-        let mut queue: VecDeque<(Option<ElementId>, Option<Index>)> = VecDeque::new();
+        struct WorkItem {
+            element: Option<ElementId>,
+            dom_node: Option<Index>,
+            parent: Option<Index>,
+        }
+
+        let mut queue: VecDeque<WorkItem> = VecDeque::new();
 
         let snapshot_roots = snapshot.roots.iter().copied();
         let dom_roots = self.roots.iter().copied();
-        queue.extend(zip(snapshot_roots, dom_roots));
+        let work = zip(snapshot_roots, dom_roots).map(|(element, dom_node)| WorkItem {
+            element,
+            dom_node,
+            parent: None,
+        });
+        queue.extend(work);
 
         // Modifications
-        while let Some((element_id, dom_index)) = queue.pop_front() {
-            match (element_id, dom_index) {
+        while let Some(work_item) = queue.pop_front() {
+            match (work_item.element, work_item.dom_node) {
                 // Updated
                 (Some(element_id), Some(dom_index)) => {
                     let element = snapshot.get(element_id).unwrap();
@@ -61,35 +72,69 @@ impl Dom {
                     } else {
                         // Because this component has changed types, delete the
                         // old one and create a new one immediately after.
-                        queue.push_front((Some(element_id), None));
-                        queue.push_front((None, Some(dom_index)));
+                        queue.push_front(WorkItem {
+                            element: Some(element_id),
+                            dom_node: None,
+                            parent: work_item.parent,
+                        });
+                        queue.push_front(WorkItem {
+                            element: None,
+                            dom_node: Some(dom_index),
+                            parent: work_item.parent,
+                        });
                     }
 
                     // Zip the children together and queue them for processing.
                     let element_children = element.children.iter().copied();
                     let dom_node_children = dom_node.children.iter().copied();
-                    queue.extend(zip(element_children, dom_node_children));
+
+                    let work =
+                        zip(element_children, dom_node_children).map(|(element, dom_node)| {
+                            WorkItem {
+                                element,
+                                dom_node,
+                                parent: Some(dom_index),
+                            }
+                        });
+                    queue.extend(work);
                 }
 
                 // Added
                 (Some(element_id), None) => {
                     let element = snapshot.get(element_id).unwrap();
 
-                    if let Some(component_impl) = self.registry.get_by_id(element.type_id) {
-                        let component = (component_impl.new)(element.props.as_ref());
+                    let index =
+                        if let Some(component_impl) = self.registry.get_by_id(element.type_id) {
+                            let component = (component_impl.new)(element.props.as_ref());
 
-                        assert_eq!(component.as_ref().type_id(), element.type_id);
+                            assert_eq!(component.as_ref().type_id(), element.type_id);
 
-                        self.tree.insert(Node {
-                            component,
-                            children: Vec::new(),
-                        });
-                    } else {
-                        panic!("Unknown component ID {:?}", element.type_id);
+                            self.tree.insert(Node {
+                                component,
+                                children: Vec::new(),
+                            })
+                        } else {
+                            panic!("Unknown component ID {:?}", element.type_id);
+                        };
+
+                    match work_item.parent {
+                        Some(parent_index) => {
+                            let parent = self.tree.get_mut(parent_index).unwrap();
+                            parent.children.push(index);
+                        }
+
+                        None => {
+                            self.roots.push(index);
+                        }
                     }
 
                     // Queue all of the element's children for addition.
-                    queue.extend(element.children.iter().copied().map(|id| (Some(id), None)));
+                    let work = element.children.iter().copied().map(|id| WorkItem {
+                        element: Some(id),
+                        dom_node: None,
+                        parent: Some(index),
+                    });
+                    queue.extend(work);
                 }
 
                 // Removed
@@ -97,7 +142,12 @@ impl Dom {
                     let node = self.tree.remove(dom_index).unwrap();
 
                     // Queue all of this node's children for removal.
-                    queue.extend(node.children.into_iter().map(|index| (None, Some(index))));
+                    let work = node.children.into_iter().map(|index| WorkItem {
+                        element: None,
+                        dom_node: Some(index),
+                        parent: Some(dom_index),
+                    });
+                    queue.extend(work);
                 }
 
                 (None, None) => unreachable!(),
