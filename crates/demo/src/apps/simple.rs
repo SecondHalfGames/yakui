40044d1c3c95d8d4ec6e059338faa2b1c2f8fa69
Lucien Greathouse <me@lpghatguy.com>
2022-07-10 00:34:56 -0400

    Make demos better

--- crates/demo/src/apps/simple.rs
+++ crates/demo/src/apps/simple.rs
@@ -13,13 +13,13 @@ pub fn app(_state: &AppState) {
             let padding = Padding::even(8.0);
             yakui::pad(padding, || {
                 yakui::row(|| {
-                    yakui::button(Button::styled([40.0, 60.0]));
-                    yakui::button(Button::styled([40.0, 60.0]));
-                    yakui::button(Button::styled([40.0, 60.0]));
+                    yakui::button(Button::styled([80.0, 30.0]));
+                    yakui::button(Button::styled([40.0, 30.0]));
+                    yakui::button(Button::styled([60.0, 30.0]));
                 });
             });
         });
 
-        yakui::button(Button::styled([20.0, 50.0]));
+        yakui::button(Button::styled([90.0, 30.0]));
     });
 }
--- crates/demo/src/apps/text.rs
+++ crates/demo/src/apps/text.rs
@@ -2,6 +2,11 @@ use crate::AppState;
 
 pub fn app(_state: &AppState) {
     yakui::column(|| {
-        yakui::text(32.0, "Hello, world!");
+        yakui::row(|| {
+            yakui::text(32.0, "Hello, world!");
+            yakui::text(16.0, "yakui text demo");
+        });
+
+        yakui::text(96.0, "yakui text demo!");
     });
 }
