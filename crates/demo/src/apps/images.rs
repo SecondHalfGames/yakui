ed5cf3d5aedd3a03baa18f3a60fec692d96f9d18
Lucien Greathouse <me@lpghatguy.com>
2022-07-10 04:49:46 -0400

    Update images demo once more

--- crates/demo/src/apps/images.rs
+++ crates/demo/src/apps/images.rs
@@ -4,11 +4,8 @@ use crate::AppState;
 
 pub fn app(state: &AppState) {
     yakui::center(|| {
-        yakui::column(|| {
-            if state.time.sin() > 0.0 {
-                yakui::text(32.0, format!("{}", state.time.sin()));
-                yakui::image(state.monkey, Vec2::new(400.0, 400.0));
-            }
-        });
+        if f32::sin(state.time * 10.0) > 0.0 {
+            yakui::image(state.monkey, Vec2::new(400.0, 400.0));
+        }
     });
 }
