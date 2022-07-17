1cbd0e576f097dc32a4171697c5861b1ef36bc7e
Lucien Greathouse <me@lpghatguy.com>
2022-07-17 02:41:35 -0400

    Add outline shape

--- crates/yakui-widgets/src/icons.rs
+++ crates/yakui-widgets/src/icons.rs
@@ -27,11 +27,9 @@ pub fn cross(output: &mut PaintDom, rect: Rect, color: Color3) {
         // '\' part of the X
         0, 2, 5,
         5, 7, 0,
-
         // bottom left square
         9, 3, 4,
         4, 10, 9,
-
         // top right square
         1, 8, 11,
         11, 6, 1,
@@ -43,6 +41,58 @@ pub fn cross(output: &mut PaintDom, rect: Rect, color: Color3) {
         .into_iter()
         .map(Vec2::from)
         .map(|pos| Vertex::new(rect.pos() + pos * rect.size(), [0.0, 0.0], color));
+
+    let mesh = PaintMesh::new(vertices, INDICES);
+    output.add_mesh(mesh);
+}
+
+pub fn outline(output: &mut PaintDom, rect: Rect, thickness: Vec2, color: Color3) {
+    let vw = rect.size().x;
+    let vh = rect.size().y;
+    let w = thickness.x;
+    let h = thickness.y;
+
+    #[rustfmt::skip]
+    let positions: [[f32; 2]; 12] = [
+        // Main rectangle
+        [0.0, 0.0], // 0
+        [0.0, vh], // 1
+        [ vw, vh], // 2
+        [ vw, 0.0], // 3
+        // Left border
+        [0.0,      h], // 4
+        [0.0, vh - h], // 5
+        [  w, vh - h], // 6
+        [  w,      h], // 7
+        // Right border
+        [vw - w,      h], // 8
+        [vw - w, vh - h], // 9
+        [    vw, vh - h], // 10
+        [    vw,      h], // 11
+    ];
+
+    #[rustfmt::skip]
+    static INDICES: [u16; 24] = [
+        // Top
+        0, 4, 11,
+        11, 3, 0,
+        // Bottom
+        5, 1, 2,
+        2, 10, 5,
+        // Left
+        4, 5, 6,
+        6, 7, 4,
+        // Right
+        8, 9, 10,
+        10, 11, 8,
+    ];
+
+    let color = color.to_linear().extend(1.0);
+
+    let vertices = positions
+        .into_iter()
+        .map(Vec2::from)
+        .map(|pos| Vertex::new(rect.pos() + pos, [0.0, 0.0], color));
 
     let mesh = PaintMesh::new(vertices, INDICES);
     output.add_mesh(mesh);
