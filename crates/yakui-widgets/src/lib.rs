e6ba7819687d09dc507cf30a991ec10bf8db1797
Lucien Greathouse <me@lpghatguy.com>
2022-07-16 23:33:35 -0400

    Move widgets into yakui-widgets/widgets

--- crates/yakui-widgets/src/lib.rs
+++ crates/yakui-widgets/src/lib.rs
@@ -3,34 +3,12 @@
                             // out our Response type even if it's unit.
 )]
 
-mod align;
-mod button;
-mod colored_box;
-mod constrained_box;
-mod flex;
-mod image;
-mod list;
-mod padding;
-mod text;
 mod text_renderer;
 mod util;
-mod window;
 
 pub mod colors;
 pub mod shorthand;
+pub mod widgets;
 
 #[doc(inline)]
 pub use self::shorthand::*;
-
-pub mod widgets {
-    pub use crate::align::*;
-    pub use crate::button::*;
-    pub use crate::colored_box::*;
-    pub use crate::constrained_box::*;
-    pub use crate::flex::*;
-    pub use crate::image::*;
-    pub use crate::list::*;
-    pub use crate::padding::*;
-    pub use crate::text::*;
-    pub use crate::window::*;
-}
---
+++ crates/yakui-widgets/src/widgets/mod.rs
@@ -0,0 +1,21 @@
+mod align;
+mod button;
+mod colored_box;
+mod constrained_box;
+mod flex;
+mod image;
+mod list;
+mod padding;
+mod text;
+mod window;
+
+pub use self::align::*;
+pub use self::button::*;
+pub use self::colored_box::*;
+pub use self::constrained_box::*;
+pub use self::flex::*;
+pub use self::image::*;
+pub use self::list::*;
+pub use self::padding::*;
+pub use self::text::*;
+pub use self::window::*;
