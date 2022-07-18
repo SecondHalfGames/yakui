3e16d9f2df7b7be1598382616deab3e233448597
Lucien Greathouse <me@lpghatguy.com>
2022-07-17 23:18:29 -0400

    Improve inputs example

--- crates/demo/src/examples/inputs.rs
+++ crates/demo/src/examples/inputs.rs
@@ -3,8 +3,12 @@ use crate::ExampleState;
 pub fn run(state: &mut ExampleState) {
     yakui::center(|| {
         yakui::column(|| {
-            yakui::button("Button");
+            if yakui::button("Button").clicked {
+                println!("Button clicked");
+            }
+
             state.checked = yakui::checkbox(state.checked).checked;
+
             state.name = yakui::textbox(18.0, state.name.clone()).text.clone();
         });
     });
