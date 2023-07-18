a5083252d7ef6ff4f59feabecd11671d3842a8a5
Lucien Greathouse <me@lpghatguy.com>
2023-07-18 19:35:24 -0400

    Expose yakui_widgets::shapes

--- crates/yakui/src/lib.rs
+++ crates/yakui/src/lib.rs
@@ -6,6 +6,7 @@ pub use yakui_widgets::widgets;
 
 pub use yakui_widgets::colors;
 pub use yakui_widgets::font;
+pub use yakui_widgets::shapes;
 pub use yakui_widgets::shorthand::*;
 pub use yakui_widgets::style;
 pub use yakui_widgets::util;
