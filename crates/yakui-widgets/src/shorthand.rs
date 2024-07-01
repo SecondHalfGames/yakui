522e1f799c6acd69a3e94754c9e86e40b5f15ff7
Madeline Sparkles <madeline@mouse.lgbt>
2024-07-02 00:19:06 +0800

    implement cosmic_text as the text renderer and editor

--- crates/yakui-widgets/src/shorthand.rs
+++ crates/yakui-widgets/src/shorthand.rs
@@ -101,7 +101,7 @@ pub fn label<S: Into<Cow<'static, str>>>(text: S) -> Response<TextResponse> {
 
 /// See [TextBox].
 pub fn textbox<S: Into<String>>(text: S) -> Response<TextBoxResponse> {
-    TextBox::new(text.into()).show()
+    TextBox::new(text).show()
 }
 
 /// See [Flexible].
